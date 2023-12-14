478263fad4b5be13b6547acb3d8a6f6139689919
Username0x29A <tomasac.22@gmail.com>
2023-12-14 17:26:03 +0100

    Update Interface_animal.kt

--- Kotlin/Interface_animal.kt
+++ Kotlin/Interface_animal.kt
@@ -36,9 +36,17 @@ fun main() {
     d.feedAnimal()
     c.petAnimal()
     d.petAnimal()
-    
     println("Cat is an animal -> ${c is Animal}")
     println("Dog is an animal -> ${d is Animal}")
+
+    /**
+    Feeding the cat
+    Feeding the dog
+    Petting a Scottish fold Cat
+    This petting method is overriden in Dog
+    Cat is an animal -> true
+    Dog is an animal -> true
+    **/
 }
 
 
