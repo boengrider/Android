61bed58fff361b0f37bc8fa8fb97a74c8edf8a40
Username0x29A <tomasac.22@gmail.com>
2023-12-14 17:24:13 +0100

    Update Interface_animal.kt

--- Kotlin/Interface_animal.kt
+++ Kotlin/Interface_animal.kt
@@ -1,1 +1,45 @@
+interface Animal {
+    val animal: String // Abstract. Override in the class
+    val breed: String // Abstract. Override in the class
+    
+    fun petAnimal() {
+        println("Petting a ${breed} ${animal}")
+    }
+    
+    fun feedAnimal() // Abstract. Override in the class
+}
+
+class Dog(override val breed: String) : Animal {
+   override val animal: String = "Dog"
+    
+   override fun feedAnimal() {
+       println("Feeding the dog")
+   }
+   
+   override fun petAnimal() = println("This petting method is overriden in Dog")
+}
+
+class Cat(override val breed: String ) : Animal {
+    override val animal: String = "Cat"
+    
+    override fun feedAnimal() {
+        println("Feeding the cat")
+    }
+}
+
+fun main() {
+    
+    val c = Cat("Scottish fold")
+    val d = Dog("Husky")
+    
+    c.feedAnimal()
+    d.feedAnimal()
+    c.petAnimal()
+    d.petAnimal()
+    
+    println("Cat is an animal -> ${c is Animal}")
+    println("Dog is an animal -> ${d is Animal}")
+}
+
+
 
