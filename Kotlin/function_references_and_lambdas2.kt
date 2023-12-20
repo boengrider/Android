ecf8cdb4e0b4d8272340adef10e6f27b88c9e343
Username0x29A <tomasac.22@gmail.com>
2023-12-20 11:05:17 +0100

    Update function_references_and_lambdas2.kt

--- Kotlin/function_references_and_lambdas2.kt
+++ Kotlin/function_references_and_lambdas2.kt
@@ -1,1 +1,25 @@
+import kotlin.math.*
+// Named lambda
+val opIncrement = { x: Int -> var tmp: Int = x; ++tmp}
 
+fun main() {
+    
+    // Apply the operation
+	var result: Int = transform(10) {
+        var tmp: Int = it
+        ++tmp
+    }
+    println(result) // 11
+    
+    // Apply the operation
+    result = transform(11, opIncrement)
+    println(result) // 12
+    
+    
+}
+
+fun transform(input: Int, operation: (Int) -> Int): Int {
+      var tmp: Int = operation(input)
+      
+      return tmp
+}
