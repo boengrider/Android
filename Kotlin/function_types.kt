ca6adc4f03843a056bc78a5b4ba5bba34a3e559f
Username0x29A <tomasac.22@gmail.com>
2024-01-03 17:24:57 +0100

    Update function_types.kt

--- Kotlin/function_types.kt
+++ Kotlin/function_types.kt
@@ -24,15 +24,26 @@ fun main() {
     // Nullables
     
     // There is no such action as '**', therefore decideActionNullable() will return null
-    val actionSquareOrNull: ((Int) -> Int)? = decideActionFunctionTypeOrNull("**")
+    val actionFunctionTypeOrNull: ((Int) -> Int)? = decideActionFunctionTypeOrNull("**")
     
-    if (actionSquareOrNull == null) println("No such action exists") //No such action exists
+   
+    println(actionFunctionTypeOrNull?.invoke(10))
+    println(actionFunctionTypeOrNull.toString())
     
     
-    // Even though there is '*=' action (square), that function-type itself can return null
+    
+    
+    
+    
+    
+    
+    
+    // Even though there is '*=' action (square), and
+    // function decideActionFunctionTypeResultOrNull will
+    // indeed return a function-type that function-type itself can return null
     // e.g. if parameter passed to the function-type itself is zero
-    val actionSquareActionResultOrNull: ((Int) -> Int?)? = decideActionFunctionTypeResultOrNull("*=")
-    val result: Int? = actionSquareActionResultOrNull?.invoke(0)
+    val actionFunctionTypeResultOrNull: (Int) -> Int? = decideActionFunctionTypeResultOrNull("*=")
+    val result: Int? = actionFunctionTypeResultOrNull(0)
     println((result == null)) // true
   
     
@@ -40,7 +51,13 @@ fun main() {
 }
 
 
-//decideAction returns a funtion type
+/***********************************
+ * Return a function-type
+ * Even if specified action doesn't
+ * exist, return function type
+ * which simply return it's parameter
+ * unmodified
+ * *********************************/
 fun decideAction(action: String): (Int) -> Int {
     
   if(action == "++") return { input: Int -> input + 1 } // Increment
@@ -54,11 +71,12 @@ fun decideAction(action: String): (Int) -> Int {
 }
 
 /***********************************
-Return a function-type OR null
-((Int) -> Int)? means, either return 
-a function-type with this signature
-else return a null 
-************************************/
+ * Return a function-type OR null
+ * ((Int) -> Int)? means, either return 
+ * a function-type with this signature
+ * or in the case action doesn't exist
+ * return null
+ ***********************************/
 
 fun decideActionFunctionTypeOrNull(action: String): ((Int) -> Int)? {
     
@@ -73,7 +91,20 @@ fun decideActionFunctionTypeOrNull(action: String): ((Int) -> Int)? {
 }
 
 
-fun decideActionFunctionTypeResultOrNull(action: String): ((Int) -> Int?)? {
+fun decideActionFunctionTypeResultOrNull(action: String): (Int) -> Int? {
+    
+  if(action == "++") return { input: Int -> input + 1 } // Increment
+  
+  if(action == "*=") return { input: Int -> if(input != 0) { input * input } else { null } } // Square
+  
+  if(action == "--") return { input: Int -> input - 1 } // Decrement
+
+  // Here we can decide, either return a unmodified non-null value or null
+  return { input: Int -> input } // No such action exists
+ 
+}
+
+fun decideActionFunctionTypeOrNullResultOrNull(action: String): ((Int) -> Int?)? {
     
   if(action == "++") return { input: Int -> input + 1 } // Increment
   
@@ -82,7 +113,6 @@ fun decideActionFunctionTypeResultOrNull(action: String): ((Int) -> Int?)? {
   if(action == "--") return { input: Int -> input - 1 } // Decrement
   
   return null // No such action exists
-  
 }
 
 
