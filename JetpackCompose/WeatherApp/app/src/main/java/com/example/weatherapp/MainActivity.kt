80dab2b0ecebfeb711d0ba2aacf5347140f60466
Username0x29A <ripper120@gmail.com>
2024-04-30 13:19:55 +0200

    a

--- JetpackCompose/WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ JetpackCompose/WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.weatherapp
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.TextView
 import com.example.weatherapp.databinding.ActivityMainBinding
 import android.net.Network
+import android.net.Uri
+import android.provider.Settings
 import android.util.JsonReader
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import com.google.gson.JsonParser
 import java.net.HttpURLConnection
 import java.net.URL
@@ -23,15 +33,62 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+        if (it[Manifest.permission.ACCESS_COARSE_LOCATION] == true && it[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
+        } else {
+            showRationalDialogForPermissions()
+        }
+    }
 
+    /**
+    override fun onResume() {
+        super.onResume()
+        if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            AlertDialog.Builder(this)
+                .setMessage("Permission removed. Enable under Application Settings")
+                .setPositiveButton("Go to settings") {
+                        _, _ ->
+                    try {
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val uri = Uri.fromParts("package", packageName, null)
+                        intent.data = uri
+                        startActivity(intent)
+                    } catch (e: ActivityNotFoundException) {
+                        e.printStackTrace()
+                    }
+                }
+                .setNegativeButton("Cancel") {
+                        dialog, _ ->
+                    dialog.dismiss()
+                }.show()
+        }
+    }
+    **/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+
+
+
+        /**
+        if(!isLocationEnabled()) Toast.makeText(this, "Your location provider is turned off", Toast.LENGTH_SHORT).show()
 
         getWeather(binding.weatherTw)
 
         binding.reloadTw.setOnClickListener {
             getWeather(binding.weatherTw)
+        }
+        **/
+
+        //Not previously granted or revoked. Request permissions
+        if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            Log.d("debug", "Permissions not granted or revoked. Requesting permissions")
+            requestPermissionLauncher.launch(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+                                             Manifest.permission.ACCESS_FINE_LOCATION))
+        } else {
+            Log.d("debug", ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION).toString())
+            Log.d("debug", ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION).toString())
         }
     }
 
@@ -64,6 +121,56 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+    }
+
+    @SuppressLint("NewApi")
+    private fun checkLocationPermission() {
+        when {
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED -> {
+                //check if the permission has been denied before and show the rationale dialog
+            }
+
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION) -> {
+                showRationalDialogForPermissions()
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+                                                         Manifest.permission.ACCESS_FINE_LOCATION))
+            }
+        }
+    }
+
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("Permission removed. Enable under Application Settings")
+            .setPositiveButton("Go to settings") {
+                _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") {
+                dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+    // Check is location provider is enabled
+    private fun isLocationEnabled(): Boolean {
+
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED
     }
 }
 
